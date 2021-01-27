ca753cb40a40db9f3f4046cb64f19f65d15b0d65
av3dj <av3dj@virginia.edu>
2021-01-27 15:52:27 -0500

    fixed AuthCode

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -8,36 +8,18 @@ const saySomething = (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
-    const options = {
-        hostname: "accounts.spotify.com",
-        path: "/authorize",
-        method: "GET",
-    }
     const data = {
         client_id: req.app.get("client_id"),
         scope: encodeURIComponent(["user-read-email"]),
-        reponse_type: "code",
+        response_type: "code",
         redirect_uri: encodeURIComponent("http://localhost:5000/api/check-login"),
     }
-    console.log(data.redirect_uri);
     const URL = `https://accounts.spotify.com/authorize`
         + `?client_id=${data.client_id}`
-        + `&reponse_type=${data.reponse_type}`
+        + `&response_type=${data.response_type}`
         + `&redirect_uri=${data.redirect_uri}`
         + `&scope=${data.scope}`
-    const config = {
-        headers: {
-        }
-    }
-    let spotify_response = await axios.get(URL)
-    .then((response) => {
-        return response;
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    res.status(200).json(spotify_response);
-
+    res.redirect(URL);
 };
 
 const checkLogin = (req, res, next) => {

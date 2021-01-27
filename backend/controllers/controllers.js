9ca912ad792c41c763fa3c5388e34bdb2d4a3996
Brian Schnee <brian.a.schnee@gmail.com>
2021-01-27 15:55:34 -0500

    Formatting

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,31 +1,33 @@
-const axios = require("axios");
-const { get } = require("../routes/routes");
+const axios = require('axios');
+const { get } = require('../routes/routes');
 
 const saySomething = (req, res, next) => {
-    res.status(200).json({
-        body: 'Hello from the server!'
-    });
+  res.status(200).json({
+    body: 'Hello from the server!',
+  });
 };
 
 const login = async (req, res, next) => {
-    const data = {
-        client_id: req.app.get("client_id"),
-        scope: encodeURIComponent(["user-read-email"]),
-        response_type: "code",
-        redirect_uri: encodeURIComponent("http://localhost:5000/api/check-login"),
-    }
-    const URL = `https://accounts.spotify.com/authorize`
-        + `?client_id=${data.client_id}`
-        + `&response_type=${data.response_type}`
-        + `&redirect_uri=${data.redirect_uri}`
-        + `&scope=${data.scope}`
-    res.redirect(URL);
+  const data = {
+    client_id: req.app.get('client_id'),
+    scope: encodeURIComponent(['user-read-email']),
+    response_type: 'code',
+    redirect_uri: encodeURIComponent('http://localhost:5000/api/check-login'),
+  };
+
+  const URL =
+    'https://accounts.spotify.com/authorize' +
+    `?client_id=${data.client_id}` +
+    `&response_type=${data.response_type}` +
+    `&redirect_uri=${data.redirect_uri}` +
+    `&scope=${data.scope}`;
+  res.redirect(URL);
 };
 
 const checkLogin = (req, res, next) => {
-    res.status(200).json({
-        body: "you have logged in"
-    });
+  res.status(200).json({
+    body: 'you have logged in',
+  });
 };
 
 module.exports.saySomething = saySomething;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
-router.get("/login", controllers.login);
-
+router.get('/login', controllers.login);
 router.get('/say-something', controllers.saySomething);
-
 router.get('/check-login', controllers.checkLogin);
+
 module.exports = router;
